ea091c10ebd10c0dd065c7673c5f92c2887b3c52
Georg <giorgio.manenti@gmail.com>
2015-07-22 23:29:46 +0200

    first_release

---
+++ Debug/main.pp
@@ -0,0 +1,39 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdint.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdbool.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/yvals.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdarg.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/linkage.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/_lock.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/math.h
+main.obj: C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/ymath.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_memmap.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_types.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/sysctl.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/fpu.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/gpio.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/debug.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/pwm.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/pin_map.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_gpio.h
+
+../main.c: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdint.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdbool.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/yvals.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdarg.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/linkage.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/_lock.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/math.h: 
+C:/ti/ccsv5/tools/compiler/arm_5.0.4/include/ymath.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/fpu.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/debug.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/pwm.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/pin_map.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_gpio.h: 
---
+++ main.c
@@ -0,0 +1,88 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <math.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/fpu.h"
+#include "driverlib/gpio.h"
+#include "driverlib/debug.h"
+#include "driverlib/pwm.h"
+#include "driverlib/pin_map.h"
+#include "inc/hw_gpio.h"
+
+
+#define MAIN_LIGHT_PORT  	GPIO_PORTF_BASE
+#define PED_LIGHT_PORT		GPIO_PORTG_BASE
+
+#define mainRed				GPIO_PIN_1
+#define mainYellow			GPIO_PIN_2
+#define mainGreen			GPIO_PIN_3
+
+#define pedRed				GPIO_PIN_0
+#define pedGreen			GPIO_PIN_1
+
+//*****************************************************************************
+//
+// The error routine that is called if the driver library encounters an error.
+//
+//*****************************************************************************
+#ifdef DEBUG
+void
+__error__(char *pcFilename, uint32_t ui32Line)
+{
+}
+#endif
+
+
+//*****************************************************************************
+//
+// System clock rate in Hz.
+//
+//*****************************************************************************
+uint32_t g_ui32SysClock;
+
+
+
+void delayMS(int ms) {
+    //ROM_SysCtlDelay( (ROM_SysCtlClockGet()/(3*1000))*ms ) ;  // more accurate
+    SysCtlDelay( (g_ui32SysClock/(3*1000))*ms ) ;  // less accurate
+}
+
+int main(void)
+{
+		//
+	    // Run from the PLL at 120 MHz.
+	    //
+	    g_ui32SysClock = SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ |
+	                SYSCTL_OSC_MAIN | SYSCTL_USE_PLL |
+	                SYSCTL_CFG_VCO_480), 120000000);
+
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOG);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOJ);
+
+	GPIOPinTypeGPIOOutput(MAIN_LIGHT_PORT, mainRed|mainYellow|mainGreen);
+	GPIOPinTypeGPIOOutput(PED_LIGHT_PORT,  pedGreen|pedRed);
+
+
+	GPIOPinWrite(MAIN_LIGHT_PORT, mainRed|mainYellow|mainGreen, 0x00);		// assure the red and blue LEDs are off
+	GPIOPinWrite(PED_LIGHT_PORT,  pedGreen|pedRed, 0x00);
+
+
+	while(1)
+	{
+		GPIOPinWrite(MAIN_LIGHT_PORT, mainRed|mainYellow|mainGreen, 0x0E);		// assure the red and blue LEDs are off
+		GPIOPinWrite(PED_LIGHT_PORT,  pedGreen|pedRed, 0x03);
+
+		delayMS(500);
+
+		GPIOPinWrite(MAIN_LIGHT_PORT, mainRed|mainYellow|mainGreen, 0x00);		// assure the red and blue LEDs are off
+		GPIOPinWrite(PED_LIGHT_PORT,  pedGreen|pedRed, 0x00);
+
+		delayMS(500);
+
+	}
+}
+
+
